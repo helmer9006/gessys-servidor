5ff7eeee181c783863ea1dec6a618d9a75f89a46
helmer9006 <59346451+helmer9006@users.noreply.github.com>
2021-04-18 21:30:47 -0500

    ajuste en router nuevos campos, error en importación

--- routes/nuevosCampos.js
+++ routes/nuevosCampos.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const nuevosCamposController = require("../controllers/nuevosCamposController");
+const nuevosCamposController = require("../controllers/NuevosCamposController");
 const { check } = require("express-validator");
 const auth = require("../middleware/auth");
 
