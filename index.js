535d93da90733a9f4d6012125efce0fad02710aa
helmer9006 <59346451+helmer9006@users.noreply.github.com>
2021-04-24 22:35:58 -0500

    ajustes cors

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ conectarDB();
 
 // Habilitar Cors
 app.use(cors());
+
+var whitelist = ['https://gessys.netlify.app']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
+//app.get('/', cors(corsOptions), (req, res) =>{
+
+
+// app.use(cors());
 // const opcionesCors = {
 //   origin: process.env.FRONTEND_URL,
 // };
@@ -34,20 +51,20 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 
 // Rutas de la app
-app.use("/api/usuarios", require("./routes/usuarios"));
-app.use("/api/categorias", require("./routes/categorias"));
-app.use("/api/dependencias", require("./routes/dependencias"));
-app.use("/api/tickets", require("./routes/tickets"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/mensajes", require("./routes/mensajes"));
-app.use("/api/inventario", require("./routes/inventario"));
-app.use("/api/proveedores", require("./routes/proveedores"));
-app.use("/api/historial-inventario", require("./routes/HistoInventario"));
-app.use("/api/tipo-inventario", require("./routes/tipoInventario"));
-app.use("/api/nuevos-campos", require("./routes/nuevosCampos"));
-app.use("/api/archivos", require("./routes/archivos"));
-app.use("/public", express.static(`${__dirname}/uploads`)); // Habilitar carpeta publica
-app.use("/api/dashboard", require("./routes/dashboard"));
+app.use("/api/usuarios", cors(corsOptions), require("./routes/usuarios"));
+app.use("/api/categorias",cors(corsOptions), require("./routes/categorias"));
+app.use("/api/dependencias", cors(corsOptions),require("./routes/dependencias"));
+app.use("/api/tickets", cors(corsOptions),  require("./routes/tickets"));
+app.use("/api/auth", cors(corsOptions), require("./routes/auth"));
+app.use("/api/mensajes", cors(corsOptions), require("./routes/mensajes"));
+app.use("/api/inventario",cors(corsOptions), require("./routes/inventario"));
+app.use("/api/proveedores", cors(corsOptions), require("./routes/proveedores"));
+app.use("/api/historial-inventario",cors(corsOptions), require("./routes/HistoInventario"));
+app.use("/api/tipo-inventario", cors(corsOptions), require("./routes/tipoInventario"));
+app.use("/api/nuevos-campos", cors(corsOptions), require("./routes/nuevosCampos"));
+app.use("/api/archivos", cors(corsOptions), require("./routes/archivos"));
+app.use("/public", cors(corsOptions), express.static(`${__dirname}/uploads`)); // Habilitar carpeta publica
+app.use("/api/dashboard", cors(corsOptions), require("./routes/dashboard"));
 
 // Arrancar la app
 app.listen(port, "0.0.0.0", () => {
