04687aa53e27887169fe4f43116ce7ef44c33326
helmer9006 <59346451+helmer9006@users.noreply.github.com>
2021-04-24 22:15:48 -0500

    ajustes cors

--- index.js
+++ index.js
@@ -9,21 +9,22 @@ const app = express();
 conectarDB();
 
 // Habilitar Cors
+app.use(cors());
 // const opcionesCors = {
 //   origin: process.env.FRONTEND_URL,
 // };
 // app.use(cors(opcionesCors));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // Puerto de la app
 const port = process.env.PORT || 4000;
